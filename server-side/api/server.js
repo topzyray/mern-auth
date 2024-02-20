937008bdeeebdd9d27ae7161f760487fc4b8eeae
topzyray <topzyray@yahoo.com>
2024-02-20 13:00:27 +0100

    Create and run the server

---
+++ server-side/api/server.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`);
+})
